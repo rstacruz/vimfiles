e8c2edf663353809fa3261a09f4de98b916cbd19
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-27 23:50:12 +0800

    Update.

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env sh
-ln -s "`pwd`/vimrc" ~/.vimrc
-mkdir -p bundle
-git clone http://github.com/gmarik/vundle.git bundle/vundle
+if [ "$(basename `pwd`)" != ".vim" ]; then
+  echo "Note: this repo must be checked out into ~/.vim."
+  exit 61
+fi
+
+if [ -f ~/.vimrc ]; then
+  echo " * Moving aside your .vimrc to .vimrc.old..."
+  mv ~/.vimrc ~/.vimrc.old
+fi
+
+echo " * Installing ~/.vimrc..."
+ln -s "`pwd -LP`/vimrc" ~/.vimrc
+
+if [ ! -e "./bundle/vundle" ]; then
+  echo " * Adding Vundle..."
+  mkdir -p bundle
+  git clone http://github.com/gmarik/vundle.git bundle/vundle
+else
+  echo " * (Skipping Vundle checkout; you already have it.)"
+fi
+
+echo ""
+echo "OK! Now run :BundleInstall in Vim."
