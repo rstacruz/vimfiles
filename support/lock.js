0344fae6f384f58d7f56c55af1704ced9e03a96d
Rico Sta. Cruz <hi@ricostacruz.com>
2014-11-06 10:16:06 +0800

    Update lock script

--- support/lock.js
+++ support/lock.js
@@ -1,28 +1,66 @@
+/*
+ * makes a "locked" version of bower.json, where versions of `dependencies` are
+ * locked to be unambiguous.
+ */
+
 var Fs = require('fs');
 var Path = require('path');
+var extend = require('util')._extend;
 
-var bundlePath = 'bundle';
-
-var out = JSON.parse(Fs.readFileSync('./bower.json', 'utf-8'));
-
-var bundles = Fs.readdirSync(bundlePath);
-bundles.forEach(function (bundle) {
-  try {
-    var
-      bowerFile = Path.join(bundlePath, bundle, '.bower.json'),
-      contents = Fs.readFileSync(bowerFile, 'utf-8'),
-      data = JSON.parse(contents);
-
-    var
-      name = data.name,
-      target = data._target,
-      source = data._originalSource;
-
-    if (target === '*') target = data._resolution.commit;
-
-    out.dependencies[name] = source + "#" + target;
-  } catch (e) {
-  }
-});
+var out = readJson('bower.json');
+extend(out.dependencies, out.devDependencies || {});
+extend(out.dependencies, getLockedDependencies());
+delete out.devDependencies;
 
 console.log(JSON.stringify(out, null, 2));
+
+/*
+ * Get locked dependencies
+ */
+
+function getLockedDependencies () {
+  var dependencies = {};
+  var bundlePath = getBowerPath();
+  var bundles = Fs.readdirSync(bundlePath);
+
+  bundles.forEach(function (bundle) {
+    try {
+      var
+        bowerFile = Path.join(bundlePath, bundle, '.bower.json'),
+        data = readJson(bowerFile);
+
+      // Read the .bower.json file
+      var
+        name = data.name,
+        target = data._target,
+        source = data._originalSource,
+        res = data._resolution.type;
+
+      // if the resolution is ambiguous, lock the commit instead
+      if (res === 'branch')
+        target = data._resolution.commit;
+
+      dependencies[name] = source + "#" + target;
+    } catch (e) {}
+  });
+
+  return dependencies;
+}
+
+/*
+ * Helpers
+ */
+
+function getBowerPath () {
+  try {
+    var bowerrc = readJson('.bowerrc');
+    if (bowerrc && bowerrc.directory)
+      return bowerrc.directory;
+  } catch (e) {}
+
+  return 'bower_components';
+}
+
+function readJson (file) {
+  return JSON.parse(Fs.readFileSync(file, 'utf-8'));
+}
