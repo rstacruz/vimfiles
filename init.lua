9bed16aab50b5d0cc5645236cc37fb47513bff47
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-04-10 22:40:07 +1000

    Initial commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require "paq" {
+  "savq/paq-nvim"; -- https://github.com/savq/paq-nvim
+
+  "rstacruz/vim-microtone";
+  "neovim/nvim-lspconfig";
+  "hrsh7th/nvim-compe";
+}
+
+vim.cmd[[color microtone]]
