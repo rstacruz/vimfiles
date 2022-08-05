c27a2ca6360a210f8bea0e37d3bdce14f3283c15
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-08-05 12:41:15 +1000

    Refactor vim_settings

--- init.lua
+++ init.lua
@@ -440,58 +440,6 @@ if true then -- Vim-rooter {{{
   vim.g.rooter_patterns = { '.git', 'package.json' }
 end -- }}}
 
-if true then -- Vim settings {{{
-  vim.opt.backup = false -- No backup files
-  vim.opt.expandtab = true
-  vim.opt.fillchars = { eob = " ", vert = "▐" } -- end-of-buffer and vertical split
-  vim.opt.gdefault = true -- Search/replace uses `g` flag by default
-  vim.opt.ignorecase = true -- Case insensitive search
-  vim.opt.mouse = "a" -- Enable mouse support
-  vim.opt.number = true -- Line numbers
-  vim.opt.pumheight = 10 -- Popup menu height
-  vim.opt.scrolloff = 4 -- Scroll padding
-  vim.opt.shiftwidth = 2
-  vim.opt.showmode = false -- Don't show '-- INSERT --' in status line
-  vim.opt.signcolumn = "yes" -- Always show sign column
-  vim.opt.smartcase = true -- Case insensitive by default, unless there's uppercase
-  vim.opt.softtabstop = 2
-  vim.opt.splitbelow = true -- Vertical splits open below
-  vim.opt.splitright = true -- Horizontal splits open to the right
-  vim.opt.swapfile = false -- Don't write swap files
-  vim.opt.termguicolors = Theme.theme[1] ~= "microtone" -- Full GUI colours in terminal
-  vim.opt.wrap = false -- Word wrap
-  vim.opt.winwidth = 85 -- Auto-resize windows
-  vim.opt.winminwidth = 12
-  vim.opt.foldlevel = 99 -- Don't fold everything on first load
-
-  if not vim.fn.has("windows") then
-    vim.opt.shell = "/bin/bash"
-  end
-
-  if utils.has_pkg("which-key.nvim") then
-    vim.opt.timeoutlen = 200
-  end
-
-  if vim.fn.has("nvim-0.7") == 1 then
-    vim.opt.laststatus = 3 -- Global statusline
-    vim.opt.fillchars = {
-      eob = " ",
-      vert = "▐",
-      vertright = "▐",
-      vertleft = "▐",
-      verthoriz = "▐",
-      horizup = "━",
-      horizdown = "━",
-      horiz = "━",
-    } -- better vert characters for global statusline
-  end
-
-  -- Causes issues with plugins like which-key
-  -- if vim.fn.has("nvim-0.8") == 1 then
-  --   vim.opt.cmdheight = 0
-  -- end
-end -- }}}
-
 if true then -- Autocmds {{{
   local augroup = require("core.utils").augroup
 
@@ -546,6 +494,8 @@ function CustomiseTheme()
     cmd([[hi! NormalNC guibg=#fafafc]])
   end
 end -- }}}
+
+require("core.setup.vim_settings").setup()
 
 -- Abbreviations
 vim.defer_fn(function()
