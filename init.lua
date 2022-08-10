1807136c883aa7ab79a41c482f5cb915eaa831b2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-08-11 00:39:15 +1000

    Update init.lua (+1 more changes)

--- init.lua
+++ init.lua
@@ -180,72 +180,6 @@ end
 
 run_later(setup_gitsigns) -- }}}
 
-local function setup_telescope() -- {{{
-  local has, telescope = pcall(require, "telescope")
-  if not has then
-    return
-  end
-  local options = {
-    defaults = {
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-      },
-      prompt_prefix = "   ",
-      selection_caret = "  ",
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = "ascending",
-      layout_strategy = "horizontal",
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-          preview_width = 0.55,
-          results_width = 0.8,
-        },
-        vertical = {
-          mirror = false,
-        },
-        width = 0.87,
-        height = 0.80,
-        preview_cutoff = 120,
-      },
-      file_sorter = require("telescope.sorters").get_fuzzy_file,
-      file_ignore_patterns = { "node_modules" },
-      generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-      path_display = { "truncate" },
-      winblend = 0,
-      border = {},
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      color_devicons = true,
-      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-      -- Developer configurations: Not meant for general override
-      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-      mappings = {
-        n = { ["q"] = require("telescope.actions").close },
-      },
-    },
-  }
-
-  telescope.setup(options)
-
-  local has_fzf, _ = pcall(require, "telescope._extensions.fzf")
-  if has_fzf then
-    telescope.load_extension("fzf")
-  end
-end
-
-run_later(setup_telescope) -- }}}
-
 local function setup_nvim_gps() -- {{{
   local has, nvim_gps = pcall(require, "nvim-gps")
   if not has then
@@ -399,6 +333,7 @@ run_later(function()
   require("core.setup.nvim-autocmds").setup()
   require("core.setup.neogit").setup()
   require("core.setup.toggleterm").setup()
+  require("core.setup.telescope").setup()
   require("core.setup.cmp").setup()
   require("core.setup.which-key").setup()
   require("core.setup.zz-deferred").setup()
