7fd650ea46bd6f0412b5e530d3034b2f3563d75b
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-08-10 20:56:39 +1000

    Update bufferline styles

--- lua/core/setup/bufferline.lua
+++ lua/core/setup/bufferline.lua
@@ -18,27 +18,27 @@ local function setup()
           text_align = "left",
         },
       },
-      separator_style = "thick", -- slant | thick | thin
+      separator_style = "slant", -- slant | thick | thin
       diagnostics = "nvim_lsp",
       -- diagnostics_indicator = function(count, level)
       --   local icon = level:match("error") and "" or ""
       --   return " " .. icon .. count
       -- end,
       groups = has_groups
-        and {
-          options = {
-            toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-          },
-          items = {
-            groups.builtin.ungrouped,
-            {
-              name = "Docs",
-              matcher = function(buf)
-                return buf.filename:match("%.md") or buf.filename:match("%.txt")
-              end,
+          and {
+            options = {
+              toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+            },
+            items = {
+              groups.builtin.ungrouped,
+              {
+                name = "Docs",
+                matcher = function(buf)
+                  return buf.filename:match("%.md") or buf.filename:match("%.txt")
+                end,
+              },
             },
           },
-        },
     },
   })
 end
