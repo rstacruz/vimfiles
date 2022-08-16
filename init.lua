00945dd38761a2776edaa407cb020c6f4d7d386c
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-08-16 13:55:24 +1000

    Fix diffview and plenary issue

--- init.lua
+++ init.lua
@@ -329,6 +329,10 @@ local function packages(use)
 			"TimUntersberger/neogit",
 			requires = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
 			cmd = { "Neogit" },
+			setup = function()
+				-- diffview requires plenary to be loaded before it
+				pcall(require, "plenary")
+			end,
 			config = function()
 				require("coresetup.neogit").setup()
 			end,
