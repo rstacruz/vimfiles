9161a1018830bb60123a6d183fd49fc09d91dfc2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-08-19 10:40:32 +1000

    Update fidget settings

--- lua/coresetup/fidget.lua
+++ lua/coresetup/fidget.lua
@@ -5,6 +5,21 @@ local function setup()
 	end
 
 	local options = {
+		text = {
+			spinner = "dots", -- default: "pipe"
+		},
+		fmt = {
+			fidget = function(name, spinner)
+				return string.format("%s %s ", name, spinner)
+			end,
+			task = function(name, msg, percentage)
+				local percentage_str = percentage and string.format(" (%s%%)", percentage)
+				if msg == "Completed" then
+					return string.format("Done: %s ✓ ", name)
+				end
+				return string.format("%s %s %s ", msg, percentage_str, name)
+			end,
+		},
 	}
 
 	fidget.setup(options)
