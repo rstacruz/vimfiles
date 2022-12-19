ec0c9edacaef496031e5f376e84260147db3aa22
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-12-19 14:49:14 +1100

    Update blue theme

--- lua/core/theme-overrides.lua
+++ lua/core/theme-overrides.lua
@@ -77,6 +77,10 @@ local function apply()
 		vim.api.nvim_set_hl(0, "XxLine", { link = "VertSplit" })
 	end
 
+	if vim.tbl_contains({ "blue" }, col) then
+		vim.api.nvim_set_hl(0, "XxLine", { link = "FloatBorder" })
+	end
+
 	if vim.tbl_contains({ "github_dark", "github_dimmed" }, col) then
 		vim.api.nvim_set_hl(0, "HopNextKey", { link = "IncSearch" }) -- "Search" is too muted
 	end
