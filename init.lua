b57e4db2b54ce1fe0b653a8cb82896cec742389f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-12-23 19:44:12 +1100

    Simplify lazy setup

--- init.lua
+++ init.lua
@@ -1,25 +1,7 @@
 -- Load configuration
 BaseConfig = require("core.defaults").defaults
 require("core.nvim-options").setup()
-
--- Install Packages
-if not vim.g.hot_reload then
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not vim.loop.fs_stat(lazypath) then
-		vim.fn.system({
-			"git",
-			"clone",
-			"--filter=blob:none",
-			"--single-branch",
-			"https://github.com/folke/lazy.nvim.git",
-			lazypath,
-		})
-	end
-	vim.opt.runtimepath:prepend(lazypath)
-	require("core.lazy").setup()
-end
-
--- Things that can't be deferred
+require("core.lazy").setup()
 require("core.theme-overrides").setup()
 require("core.theme-utils").setup()
 require("core.autocmds").setup()
