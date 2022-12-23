dfebb65ee80137fbdd5af166a2744b86e145be0f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2022-12-23 23:08:16 +1100

    Add matchup

---
+++ lua/packages/matchup.lua
@@ -0,0 +1,11 @@
+-- matchup: make `%` toggle between `function` and `end`
+--
+--   %    go to pair
+--   [%   go to parent context
+--
+local M = {
+	"andymass/vim-matchup",
+	event = "VeryLazy",
+}
+
+return M
