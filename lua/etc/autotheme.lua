ca602db7d60257effa54c61930762b37dba09ca9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-03-08 20:54:00 +1100

    Fix autotheme in GNOME

--- lua/etc/autotheme.lua
+++ lua/etc/autotheme.lua
@@ -5,12 +5,32 @@
 --   light = { colorscheme = "catpuccin" },
 -- })
 
-local function get_mode_macos(callback)
-  vim.fn.jobstart("defaults read -g AppleInterfaceStyle", {
+-- Executes a command, returns { exit_code
+local function exec(cmd, callback)
+  local out = {}
+  vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    on_stdout = function(_, stdout, _)
+      for _, line in ipairs(stdout) do
+        table.insert(out, line)
+      end
+    end,
     on_exit = function(exit_code)
-      callback(exit_code == 0 and "dark" or "light")
+      callback({ exit_code = exit_code, stdout = out })
     end,
   })
+end
+
+local function get_mode_macos(callback)
+  exec("defaults read -g AppleInterfaceStyle", function(result)
+    callback(result.exit_code == 0 and "dark" or "light")
+  end)
+end
+
+local function get_mode_gnome(callback)
+  exec("dconf read /org/gnome/desktop/interface/color-scheme", function(result)
+    callback(result.stdout[1] == "'default'" and "light" or "dark")
+  end)
 end
 
 local function get_mode(callback)
@@ -18,6 +38,10 @@ local function get_mode(callback)
 
   if uname.sysname == "Darwin" then -- "Darwin" | "Linux"
     return get_mode_macos(callback)
+  end
+
+  if vim.fn.executable("dconf") == 1 then
+    return get_mode_gnome(callback)
   end
 end
 
@@ -38,5 +62,9 @@ local function setup(options)
     end
   end)
 end
+
+get_mode(function(out)
+  vim.notify("mode:" .. out)
+end)
 
 return { setup = setup }
