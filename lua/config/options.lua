877c6bd7177fa22fd09a34e9ea1bd1ba2dacb7d6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-04-09 21:46:00 +1000

    Update lua/config/options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,4 @@ vim.opt.clipboard = "" -- Don't use system clipboard
 vim.opt.swapfile = false -- Don't write swap files
 vim.opt.gdefault = true -- Searches will be global by default
 vim.opt.fillchars = "eob: " -- No ~ at end of buffer
+vim.opt.conceallevel = 0 -- Don't conceal by default
