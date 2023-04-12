5fc781ceeeffc06962bbca2fe09166e331842fd9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-04-12 22:52:35 +1000

    Update transparentize

--- plugins/transparentize/lua/transparentize.lua
+++ plugins/transparentize/lua/transparentize.lua
@@ -1,7 +1,16 @@
 local function transparentize()
-  vim.api.nvim_set_hl(0, "LineNr", { bg = "none", ctermbg = "none" })
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none", ctermbg = "none" })
-  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", ctermbg = "none" })
+  vim.cmd([[
+    hi! LineNr guibg=none ctermbg=none
+    hi! Normal guibg=none ctermbg=none
+    hi! NormalNC guibg=none ctermbg=none
+    hi! SignColumn guibg=none ctermbg=none
+    hi! VertSplit guibg=none ctermbg=none
+  ]])
+  -- We can use nvim_set_hl, but they will _override_ the whole highlight definition.
+  -- Using :highlight! will _append_ the new definition to the existing one.
+  --
+  --   vim.api.nvim_set_hl(0, "Normal", { bg = "none", ctermbg = "none" })
+  --
 end
 
 return { transparentize = transparentize }
