55ef1fe0bcd8cce813c88c0c8ee7406d5ef1c1c0
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-05-17 21:16:21 +1000

    Undo the conceal option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,7 @@ vim.opt.clipboard = "" -- Don't use system clipboard
 vim.opt.swapfile = false -- Don't write swap files
 vim.opt.gdefault = true -- Searches will be global by default
 vim.opt.fillchars = "eob: " -- No ~ at end of buffer
-vim.opt.conceallevel = 0 -- Don't conceal by default
+-- vim.opt.conceallevel = 0 -- Don't conceal by default
 
 vim.opt.formatoptions:remove({ "r", "o" })
 -- default: jcroqlnt
