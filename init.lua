3a464ffd99b2c943c01a78330df8d359c5c2d8ff
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-08-08 13:29:52 +1000

    Fix shada errors on macOS

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- prevent "cannot parse shada file" errors
+if vim.fn.has('macunix') then
+  vim.cmd("set shada+='1000,n~/.cache/nvim/shada")
+end
+
+pcall(require, "init-local")
 require("etc.theme_overrides").setup()
 require("config.lazy")
-pcall(require, "init-local")
