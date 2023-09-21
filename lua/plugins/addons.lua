bf7332e01c9f6793ad0e0daa9165bea7dbcbdd6c
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-09-21 13:43:10 +1000

    Add obsidian.nvim

--- lua/plugins/addons.lua
+++ lua/plugins/addons.lua
@@ -6,6 +6,73 @@ local uname = vim.loop.os_uname()
 local is_android = uname.machine == "aarch64"
 
 return {
+  { -- obsidian
+    -- :ObsidianOpen - open in obsidian app
+    -- :ObsidianQuickSwitch
+    -- :ObsidianFollowLink
+    -- :ObsidianBacklinks
+    -- :ObsidianSearch
+    "epwalsh/obsidian.nvim",
+    lazy = true,
+    event = vim.g.obsidian_vault_dir and {
+      "BufReadPre " .. vim.fn.expand(vim.g.obsidian_vault_dir) .. "/**.md",
+      "BufNewFile " .. vim.fn.expand(vim.g.obsidian_vault_dir) .. "/**.md",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      dir = vim.g.obsidian_vault_dir,
+      -- Disable auto-updating frontmatter id, createdAt, alias, etc
+      disable_frontmatter = true,
+      mappings = {},
+    },
+    config = function(_, opts)
+      require("obsidian").setup(opts)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("obsidian_keybindings", { clear = true }),
+        pattern = { "markdown" },
+        callback = function(event)
+          -- event = { buf, event, file, group, id, match }
+          vim.keymap.set(
+            "n",
+            "<leader>mr",
+            "<cmd>ObsidianBacklinks<CR>",
+            { buffer = event.buf, desc = "Obsidian: Show backlinks" }
+          )
+          vim.keymap.set(
+            "n",
+            "<c-p>",
+            "<cmd>ObsidianQuickSwitch<CR>",
+            { buffer = event.buf, desc = "Obsidian: Open..." }
+          )
+          vim.keymap.set(
+            "n",
+            "<leader>ms",
+            "<cmd>ObsidianQuickSwitch<CR>",
+            { buffer = event.buf, desc = "Obsidian: Open..." }
+          )
+          vim.keymap.set(
+            "n",
+            "<leader>m/",
+            "<cmd>ObsidianSearch<CR>",
+            { buffer = event.buf, desc = "Obsidian: Search..." }
+          )
+          vim.keymap.set(
+            "n",
+            "<leader>mf",
+            "<cmd>ObsidianFollowLink<CR>",
+            { buffer = event.buf, desc = "Obsidian: Follow link" }
+          )
+        end,
+      })
+    end,
+    -- TODO: add mappings for ctrl-p -> :ObsidianQuickSwitch
+    -- TODO: add mappings for gf (ObsidianFollowLink)
+    -- TODO: add mappings for gr (ObsidianBacklinks)
+  },
+
   { -- navic
     -- it's really cool af. In practice though, it never really helped me
     "SmiteshP/nvim-navic",
@@ -303,7 +370,7 @@ return {
         if client.name == "zk" then
           -- stylua: ignore start
           vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = buffer, desc = "Preview link under cursor" })
-          vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = buffer, desc = "Follow link" })
+          -- vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = buffer, desc = "Follow link" })
           vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = buffer, desc = "Follow link" })
           vim.keymap.set("n", "gr", "<cmd>ZkBacklinks<CR>", { buffer = buffer, desc = "Show backlinks (references)" })
 
