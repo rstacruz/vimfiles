68c0265925ad4acd27ca60ae041b4b5227b0ef06
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-10-17 16:16:24 +1100

    aerial: hide variables

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -13,7 +13,18 @@ return {
     "stevearc/aerial.nvim",
     opts = function(_, opts)
       opts.layout.min_width = 50
-      opts.layout.default_direction = "prefer_left"
+      -- opts.layout.default_direction = "prefer_left"
+
+      opts.filter_kind = {
+        "Class",
+        "Constructor",
+        "Enum",
+        "Function",
+        "Interface",
+        "Module",
+        "Method",
+        "Struct",
+      }
     end,
   },
 
