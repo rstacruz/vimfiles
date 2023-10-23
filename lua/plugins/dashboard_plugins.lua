aa3bc6d2596c0fc4599a81580f50eff1f2c48b9c
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-10-23 21:12:25 +1100

    Simplify dashboard again

--- lua/plugins/dashboard_plugins.lua
+++ lua/plugins/dashboard_plugins.lua
@@ -4,7 +4,8 @@ return {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     opts = function()
-      local logo = "" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+      local hr = "─────────────────────────"
+      local logo = "" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. "\n" .. hr
       logo = string.rep("\n", 8) .. logo .. "\n\n"
 
       local oldfiles = Util.telescope("oldfiles", { only_cwd = true })
@@ -19,23 +20,26 @@ return {
           header = vim.split(logo, "\n"),
           -- stylua: ignore
           center = {
-            { action = "ene | startinsert",     desc = " New file",        icon = " ", key = "e", key_format = " %s" },
-            { action = Util.telescope('files'), desc = " Open file...",    icon = " ", key = "p", key_format = " %s" },
-            { action = oldfiles,                desc = " Recent files...", icon = " ", key = "r", key_format = " %s" },
-            { action = restore_session,         desc = " Restore session", icon = " ", key = "s", key_format = " %s" },
-            { action = "qa",                    desc = " Quit",            icon = " ", key = "q", key_format = " %s" },
+            { action = "ene | startinsert",     desc = " New file",        icon = " ", key = "e" },
+            { action = Util.telescope('files'), desc = " Open file...",    icon = " ", key = "p" },
+            { action = oldfiles,                desc = " Recent files...", icon = " ", key = "r" },
+            { action = restore_session,         desc = " Restore session", icon = " ", key = "s" },
+            { action = "qa",                    desc = " Quit",            icon = " ", key = "q" },
           },
           footer = function()
             local stats = require("lazy").stats()
             local ms = math.floor((stats.startuptime * 100 + 0.5) / 100)
-            return { "" .. stats.loaded .. "/" .. stats.count .. " plugins _ " .. ms .. "ms" }
+            return { "", hr, "" .. stats.loaded .. "/" .. stats.count .. " plugins _ " .. ms .. "ms" }
           end,
         },
       }
 
-      -- for _, button in ipairs(opts.config.center) do
-      --   button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-      -- end
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 23 - #button.desc)
+        button.key_format = " %s"
+        button.key_hl = "SignColumn"
+        button.icon_hl = "SignColumn"
+      end
 
       -- close Lazy and re-open when the dashboard is ready
       if vim.o.filetype == "lazy" then
