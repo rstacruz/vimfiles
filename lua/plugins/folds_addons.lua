f677194e74aa3df3641e6c172d26814cecf6a5c3
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-10-31 12:26:07 +1100

    Update ufo settings

--- lua/plugins/folds_addons.lua
+++ lua/plugins/folds_addons.lua
@@ -1,3 +1,33 @@
+-- https://github.com/kevinhwang91/nvim-ufo#customize-fold-text
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = ("[%d]"):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { " …  ", "Comment" })
+  table.insert(newVirtText, { suffix, "DiagnosticOk" }) -- MoreMsg
+  return newVirtText
+end
+
 return {
   { -- ufo: better folds
     "kevinhwang91/nvim-ufo",
@@ -5,11 +35,69 @@ return {
       "kevinhwang91/promise-async",
     },
     lazy = true,
-    event = { "BufReadPost", "BufNew" },
-    opts = {},
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      fold_virt_text_handler = handler,
+    },
     keys = {
-      { "zR", "<cmd>lua require('ufo').openAllFolds()<cr>", desc = "Open all folds" },
-      { "zM", "<cmd>lua require('ufo').closeAllFolds()<cr>", desc = "Close all folds" },
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+        desc = "Open all folds",
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+        desc = "Close all folds",
+      },
+      -- {
+      --   "zr",
+      --   function()
+      --     require("ufo").openFoldsExceptKinds()
+      --   end,
+      --   desc = "Increase fold level",
+      -- },
+      -- {
+      --   "zm",
+      --   function()
+      --     require("ufo").closeFoldsWith()
+      --   end,
+      --   desc = "Reduce fold level",
+      -- },
+      {
+        "zk", -- "[z",
+        function()
+          require("ufo").goPreviousClosedFold()
+          vim.schedule(function()
+            require("ufo").peekFoldedLinesUnderCursor()
+          end)
+        end,
+        desc = "UFO: Peek prev fold",
+      },
+      {
+        "zj", -- "]z"
+        function()
+          require("ufo").goNextClosedFold()
+          vim.schedule(function()
+            require("ufo").peekFoldedLinesUnderCursor()
+          end)
+        end,
+        desc = "UFO: Peek next fold",
+      },
+      -- {
+      --   "K",
+      --   function()
+      --     local winid = require("ufo").peekFoldedLinesUnderCursor()
+      --     if not winid then
+      --       vim.lsp.buf.hover()
+      --     end
+      --   end,
+      --   desc = "Peek",
+      -- },
       -- also see: https://github.com/LazyVim/LazyVim/discussions/1881#discussioncomment-7395199
     },
   },
