919fba413fe468d138359323f3af52251d43220d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-12-10 10:56:24 +1100

    Update lua/plugins/markdown_plugins.lua

--- lua/plugins/markdown_plugins.lua
+++ lua/plugins/markdown_plugins.lua
@@ -77,6 +77,16 @@ return {
       attachments = {
         img_folder = "Media", -- default: "assets/imgs"
       },
+      ui = {
+        checkboxes = {
+          -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "", hl_group = "ObsidianDone" },
+          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+          ["/"] = { char = "󰡖", hl_group = "ObsidianDone" }, -- in progress
+        },
+      },
     },
     config = function(_, opts)
       require("obsidian").setup(opts)
