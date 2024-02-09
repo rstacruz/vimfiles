28a4428b14ac7de39d68c719af210bf38de6f20d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-02-09 20:46:35 +1100

    Update conceallevel for markdown

--- lua/config/autocmds_markdown.lua
+++ lua/config/autocmds_markdown.lua
@@ -7,6 +7,9 @@ M.setup = function()
     callback = function(event)
       -- event = { buf, event, file, group, id, match }
       vim.keymap.set("v", "gb", "<cmd>lua vim.notify('hi')<CR>", { buffer = event.buf, desc = "Markdown: bold" })
+
+      -- https://github.com/epwalsh/obsidian.nvim/issues/286
+      vim.opt_local.conceallevel = 2
     end,
   })
 end
