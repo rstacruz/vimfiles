f6f0f6a434b1c80000e40329f1733e11cca2eefc
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-05-07 08:13:53 +1000

    Update git-confict config

--- lua/plugins/git_addons.lua
+++ lua/plugins/git_addons.lua
@@ -6,12 +6,6 @@ return {
     opts = {},
     keys = {
       { "<leader>gxl", "<cmd>GitConflictListQf<cr>", "List git conflicts" },
-      { "<leader>gx<", "<cmd>GitConflictChooseOurs<cr>", "Choose ours" },
-      { "<leader>gx>", "<cmd>GitConflictChooseTheirs<cr>", "Choose theirs" },
-      { "<leader>gx=", "<cmd>GitConflictChooseTheirs<cr>", "Choose both" },
-      { "<leader>gx-", "<cmd>GitConflictChooseTheirs<cr>", "Choose none" },
-      { "]C", "<cmd>GitConflictNextConflict<cr>", "Next Git conflict" },
-      { "[C", "<cmd>GitConflictPrevConflict<cr>", "Prev Git conflict" },
     },
   },
 
