46d9ac83fcbd3c517ba5ba868b10018b5dc0450a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-06-03 21:51:10 +1000

    fix(autocmds): fix autoreload, remove extraneous code

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,6 @@
 
 vim.api.nvim_create_autocmd("FocusGained", {
   group = vim.api.nvim_create_augroup("autoreload_on_focus", { clear = true }),
-  pattern = { "spectre_panel" },
   callback = function()
     vim.cmd("checktime")
   end,
@@ -47,26 +46,14 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("custom_foldexpr", { clear = true }),
-  pattern = { "typescript", "javascript", "markdown", "lua", "typescriptreact", "sh" },
-  callback = function()
-    -- https://github.com/nvim-treesitter/nvim-treesitter#folding
-    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
-    vim.opt_local.foldmethod = "expr"
-    vim.opt_local.foldenable = false
-    -- vim.opt_local.formatoptions:remove({ "r", "o" })
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("custom_markdown", { clear = true }),
   pattern = { "markdown" },
   callback = function()
     vim.opt_local.cursorline = false -- doesn't look good when used with headline.nvim. toggle with leader-uL
     vim.opt_local.spell = false -- I find spellcheck only useful when writing prose. toggle with leader-us
     vim.opt_local.wrap = false -- inline links make wrapping very weird. toggle with leader-uw
-    vim.opt_local.number = false -- toggle with leader-ul
     vim.opt_local.relativenumber = false
+    vim.opt_local.number = false -- toggle with leader-ul
   end,
 })
 
