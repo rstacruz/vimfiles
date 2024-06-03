6d6a8bf5b16434eef69274f83a685ecb1964a71e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-06-03 21:38:55 +1000

    feat(markdown): add Markdown keymaps

--- lua/config/autocmds_markdown.lua
+++ lua/config/autocmds_markdown.lua
@@ -6,7 +6,21 @@ M.setup = function()
     pattern = { "markdown" },
     callback = function(event)
       -- event = { buf, event, file, group, id, match }
-      vim.keymap.set("v", "gb", "<cmd>lua vim.notify('hi')<CR>", { buffer = event.buf, desc = "Markdown: bold" })
+      vim.keymap.set(
+        "v",
+        "gb",
+        "gsa*i*<Esc>f*i*<Esc><Right>",
+        { buffer = event.buf, desc = "Markdown: bold", remap = true }
+      )
+      vim.keymap.set("v", "gi", "gsa_", { buffer = event.buf, desc = "Markdown: italic", remap = true })
+
+      -- paste link
+      vim.keymap.set(
+        "v",
+        "g+",
+        'gsa]f]a()<Esc><Left>"+p<Right>',
+        { buffer = event.buf, desc = "Markdown: paste link", remap = true }
+      )
 
       -- https://github.com/epwalsh/obsidian.nvim/issues/286
       vim.opt_local.conceallevel = 2
