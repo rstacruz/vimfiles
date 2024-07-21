3d0695fbac64858e78a7abeff38a9fe1184f6a95
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-07-22 09:38:12 +1000

    Update lazy-lock.json (+1 more changes)

--- lua/plugins/addons.lua
+++ lua/plugins/addons.lua
@@ -161,7 +161,8 @@ return {
       require("toggleterm").setup(opts)
     end,
     keys = {
-      { "<leader>.", "<cmd>ToggleTerm<cr>", desc = "Terminal" },
+      { "<leader>!.", "<cmd>ToggleTerm<cr>", desc = "Terminal (below)" },
+      { "<leader>.", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Terminal (right)" },
       { "<leader>gf", '<cmd>w | TermExec size=6 cmd="git-fastsync && exit"<cr>', desc = "Git fastsync" },
     },
   },
