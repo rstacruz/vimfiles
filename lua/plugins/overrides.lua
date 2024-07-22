858451db41682abb9533ae3dd220ada56da7a393
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-07-23 01:15:55 +1000

    Add _graveyard/plugins/2024-07-spectre.lua (+3 more changes)

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -173,39 +173,6 @@ return {
   --   },
   -- },
 
-  { -- Spectre
-    -- Opens in insert mode
-    "nvim-pack/nvim-spectre",
-    enabled = not is_vscode,
-    opts = {
-      line_sep_start = "",
-      result_padding = "   ",
-      line_sep = "",
-      find_engine = {
-        ["rg"] = {
-          -- add hidden
-          args = { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--hidden" },
-        },
-      },
-    },
-    keys = {
-      { -- Updates options passed to open()
-        "<leader>sr",
-        function()
-          require("spectre").open({ is_insert_mode = true })
-        end,
-        desc = "Replace in files (Spectre)",
-      },
-      { -- new
-        "<leader>s*",
-        function()
-          require("spectre").open_visual({ select_word = true })
-        end,
-        desc = "Replace in files (Spectre)",
-      },
-    },
-  },
-
   { -- Flit
     -- Rebinds `f F t T` to be leap-like. I can never get used to it :(
     "ggandor/flit.nvim",
