ce23c5eeb6d3b4226f24d5a16d32a008bfea7d04
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-08-13 14:03:58 +1000

    feat(markdown): fix checkboxes in wezterm

--- lua/plugins/markdown_plugins.lua
+++ lua/plugins/markdown_plugins.lua
@@ -42,20 +42,20 @@ return {
 
         checkbox = {
           custom = {
-            wait = { raw = "[-]", rendered = "󰥔", highlight = "RenderMarkdownTodo" },
-            prio = { raw = "[!]", rendered = "󰄱", highlight = "DiagnosticError" }, -- high priority
-            done = { raw = "[x]", rendered = "󰄲", highlight = "DiagnosticOk" }, -- nf-md-checkbox_marked
-            yes = { raw = "[y]", rendered = "󰄲", highlight = "DiagnosticOk" }, -- nf-md-checkbox_marked
-            later = { raw = "[>]", rendered = "󰒊", highlight = "DiagnosticInfo" }, -- nf-md-send
-            sched = { raw = "[<]", rendered = "󰃰", highlight = "DiagnosticInfo" }, -- nf-md-calendar_clock
-            cancel = { raw = "[~]", rendered = "󰂭", highlight = "Comment" },
-            info = { raw = "[i]", rendered = "󰋼", highlight = "DiagnosticInfo" }, -- nf-md-information
-            idea = { raw = "[I]", rendered = "󰌵", highlight = "DiagnosticWarn" }, -- nf-md-lightbulb
-            pro = { raw = "[p]", rendered = "󰔓", highlight = "DiagnosticOk" }, -- nf-md-thumb_up
-            con = { raw = "[c]", rendered = "󰔑", highlight = "DiagnosticError" }, -- nf-md-thumb_down
-            star = { raw = "[s]", rendered = "󰓎", highlight = "DiagnosticWarn" }, -- nf-md-star (asterisk * doesn't work)
-            star2 = { raw = "[*]", rendered = "󰓎", highlight = "DiagnosticWarn" }, -- nf-md-star (asterisk * doesn't work)
-            half = { raw = "[/]", rendered = "󰿦", highlight = "DiagnosticWarn" }, -- in progress, nf-md-texture_box
+            wait = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo" },
+            prio = { raw = "[!]", rendered = "󰄱 ", highlight = "DiagnosticError" }, -- high priority
+            done = { raw = "[x]", rendered = "󰄲 ", highlight = "DiagnosticOk" }, -- nf-md-checkbox_marked
+            yes = { raw = "[y]", rendered = "󰄲 ", highlight = "DiagnosticOk" }, -- nf-md-checkbox_marked
+            later = { raw = "[>]", rendered = "󰒊 ", highlight = "DiagnosticInfo" }, -- nf-md-send
+            sched = { raw = "[<]", rendered = "󰃰 ", highlight = "DiagnosticInfo" }, -- nf-md-calendar_clock
+            cancel = { raw = "[~]", rendered = "󰂭 ", highlight = "Comment" },
+            info = { raw = "[i]", rendered = "󰋼 ", highlight = "DiagnosticInfo" }, -- nf-md-information
+            idea = { raw = "[I]", rendered = "󰌵 ", highlight = "DiagnosticWarn" }, -- nf-md-lightbulb
+            pro = { raw = "[p]", rendered = "󰔓 ", highlight = "DiagnosticOk" }, -- nf-md-thumb_up
+            con = { raw = "[c]", rendered = "󰔑 ", highlight = "DiagnosticError" }, -- nf-md-thumb_down
+            star = { raw = "[s]", rendered = "󰓎 ", highlight = "DiagnosticWarn" }, -- nf-md-star (asterisk * doesn't work)
+            star2 = { raw = "[*]", rendered = "󰓎 ", highlight = "DiagnosticWarn" }, -- nf-md-star (asterisk * doesn't work)
+            half = { raw = "[/]", rendered = "󰿦 ", highlight = "DiagnosticWarn" }, -- in progress, nf-md-texture_box
           },
         },
 
@@ -64,9 +64,9 @@ return {
           -- RenderMarkdownSuccess - green
           -- RenderMarkdownInfo - blue
           -- RenderMarkdownError - red
-          highlights = { raw = "[!HIGHLIGHTS]", rendered = "󰌶 Highlights", highlight = "RenderMarkdownHint" },
-          tldr = { raw = "[!TLDR]", rendered = "󰌶 TLDR", highlight = "RenderMarkdownHint" },
-          summary = { raw = "[!SUMMARY]", rendered = "󰌶 Summary", highlight = "RenderMarkdownHint" },
+          highlights = { raw = "[!HIGHLIGHTS]", rendered = "󰌶 Highlights ", highlight = "RenderMarkdownHint" },
+          tldr = { raw = "[!TLDR]", rendered = "󰌶 TLDR ", highlight = "RenderMarkdownHint" },
+          summary = { raw = "[!SUMMARY]", rendered = "󰌶 Summary ", highlight = "RenderMarkdownHint" },
         },
       }
     end,
