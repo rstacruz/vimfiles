2e2c108f360e58111b167f75259982bd31bc5794
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-10-01 12:16:08 +1000

    Update lazy-lock.json (+1 more changes)

--- lua/plugins/markdown_plugins.lua
+++ lua/plugins/markdown_plugins.lua
@@ -127,4 +127,32 @@ return {
       })
     end,
   },
+
+  {
+    "zk-org/zk-nvim",
+    lazy = true,
+    ft = { "markdown" },
+    opts = {
+      picker = "telescope",
+    },
+    config = function(_, opts)
+      require("zk").setup(opts)
+      LazyVim.lsp.on_attach(function(client, buffer)
+        if client.name == "zk" then
+          -- stylua: ignore start
+          vim.keymap.set("n", "<leader>zr", "<cmd>ZkBacklinks<CR>", { buffer = buffer, desc = "Show backlinks (references)" })
+          vim.keymap.set("n", "<leader>zl", "<cmd>ZkLinks<CR>", { buffer = buffer, desc = "Show outgoing links" })
+          vim.keymap.set("n", "<leader>zL", "<cmd>ZkLinks {recursive=true}<CR>", { buffer = buffer, desc = "Show outgoing links (recursive)" })
+          vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", { buffer = buffer, desc = "Create new page…" })
+
+          -- Create a note in the same dir, using current selection for content
+          vim.keymap.set("v", "<leader>zn", "<Cmd>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", { buffer = buffer, desc = "Create new page from selection…" })
+
+          -- Create a note in the same dir, using current selection for title
+          vim.keymap.set("v", "<leader>zt", "<Cmd>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", { buffer = buffer, desc = "Link to new file…" })
+          -- stylua: ignore end
+        end
+      end)
+    end,
+  },
 }
