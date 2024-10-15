5686a3f22895a4312abb5f4ccf6d47d5260bf3a3
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-10-15 16:13:26 +1100

    feat(catppuccin): adjust frappe gamma

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -139,3 +139,37 @@ vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev bu
 -- vim.keymap.set("n", "<C-Down>", "<cmd>resize -10<cr>", { desc = "Decrease window height" })
 -- vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -10<cr>", { desc = "Decrease window width" })
 -- vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +10<cr>", { desc = "Increase window width" })
+
+if vim.g.vscode then
+  vim.keymap.set(
+    "n",
+    "]d",
+    "<cmd>lua require('vscode').call('editor.action.marker.next')<cr>",
+    { desc = "Next Diagnostic" }
+  )
+  vim.keymap.set(
+    "n",
+    "[d",
+    "<cmd>lua require('vscode').call('editor.action.marker.previous')<cr>",
+    { desc = "Prev Diagnostic" }
+  )
+  vim.keymap.set(
+    "n",
+    "gr",
+    "<cmd>lua require('vscode').call('editor.action.goToReferences')<cr>",
+    { desc = "Goto References" }
+  )
+  vim.keymap.set(
+    "n",
+    "gd",
+    "<cmd>lua require('vscode').call('editor.action.revealDefinition')<cr>",
+    { desc = "Goto Definition" }
+  )
+  vim.keymap.set(
+    "n",
+    "gy",
+    "<cmd>lua require('vscode').call('editor.action.goToTypeDefinition')<cr>",
+    { desc = "Goto Type Definition" }
+  )
+end
+
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -87,12 +87,43 @@ return {
   {
     "catppuccin/nvim",
     event = "VeryLazy",
-    opts = {
-      color_overrides = {
+    opts = function(_, opts)
+      local c = require("catppuccin.utils.colors")
+
+      opts.color_overrides = {
         latte = {
           base = "#ededf5",
         },
-      },
-    },
+
+        frappe = {
+          rosewater = c.brighten("#f4dbd6", 0.3),
+          flamingo = c.brighten("#f0c6c6", 0.3),
+          pink = c.brighten("#f5bde6", 0.3),
+          mauve = c.brighten("#c6a0f6", 0.3),
+          red = c.brighten("#ed8796", 0.3),
+          maroon = c.brighten("#ee99a0", 0.3),
+          peach = c.brighten("#f5a97f", 0.3),
+          yellow = c.brighten("#eed49f", 0.3),
+          green = c.brighten("#a6da95", 0.3),
+          teal = c.brighten("#8bd5ca", 0.3),
+          sky = c.brighten("#91d7e3", 0.3),
+          sapphire = c.brighten("#7dc4e4", 0.3),
+          blue = c.brighten("#8aadf4", 0.3),
+          lavender = c.brighten("#b7bdf8", 0.3),
+          text = c.brighten("#cad3f5", 0.3),
+          subtext1 = c.brighten("#b8c0e0", 0.1),
+          subtext0 = c.brighten("#a5adcb", 0.1),
+          overlay2 = c.brighten("#939ab7", 0.1),
+          overlay1 = c.brighten("#8087a2", 0.1),
+          overlay0 = c.brighten("#6e738d", 0.1),
+          surface2 = c.brighten("#5b6078", 0.1),
+          surface1 = c.brighten("#494d64", 0.1),
+          surface0 = c.brighten("#363a4f", 0.1),
+          base = c.brighten("#24273a", 0.1),
+          mantle = c.brighten("#1e2030", 0.1),
+          crust = c.brighten("#181926", 0.1),
+        },
+      }
+    end,
   },
 }
