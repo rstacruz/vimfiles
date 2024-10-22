5af459426b143fb3e1ba876d22d830bad3e75b0b
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-10-22 13:17:20 +1100

    Add options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- vim.opt.clipboard = "" -- Don't use system clipboard
+vim.opt.swapfile = false -- Don't write swap files
+vim.opt.gdefault = true -- Searches will be global by default
+vim.opt.relativenumber = false
+-- vim.opt.laststatus = 3
+
+-- vim.opt.formatoptions:remove({ "r", "o" })
+-- default: jcroqlnt
+-- formatoptions-=r -- don't auto-insert comment leader on enter
+-- formatoptions-=o -- don't auto-insert comment leader on o/O
