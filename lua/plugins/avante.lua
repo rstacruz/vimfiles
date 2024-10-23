ceb87ec4f82042990d8cb634effb22ad151cf757
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-10-24 10:25:29 +1100

    Disable avante when copilot is not setup

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,11 +1,12 @@
 local is_termux = string.find(vim.loop.os_uname().release, "android")
+local has_copilot_setup = vim.fn.filereadable(vim.fn.expand("~/.config/github-copilot/hosts.json")) == 1
 
 -- https://github.com/LazyVim/LazyVim/pull/4440
 return {
   {
     "yetone/avante.nvim",
     vscode = false,
-    enabled = not is_termux,
+    enabled = (not is_termux) and has_copilot_setup,
     init = function()
       require("avante_lib").load()
     end,
