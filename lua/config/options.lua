bb38dc083ab270c8c04d0b3d7ccd931524347c47
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-10-29 22:32:50 +1100

    Don't use system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.opt.clipboard = "" -- Don't use system clipboard
+vim.opt.clipboard = "" -- Don't use system clipboard
 vim.opt.swapfile = false -- Don't write swap files
 vim.opt.gdefault = true -- Searches will be global by default
 vim.opt.relativenumber = false
