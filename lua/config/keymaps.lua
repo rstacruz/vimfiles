11f89a74c03bbd7fb16f29f298fe4d3a550e035a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-10-31 22:56:44 +1100

    feat(keymaps): add keymap to restart LSP

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
+-- Fix all the problems
+vim.keymap.set("n", "<leader>cx", "<cmd>LspRestart<cr>", { desc = "Restart LSP" })
+
 -- Code action (default: leader-ca)
 vim.keymap.set({ "n", "v" }, "g.", vim.lsp.buf.code_action, { desc = "Code Action" })
 
