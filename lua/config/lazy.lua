1dfe8566e39cd24d0870c49709cb14f3b16ba213
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-11-14 21:18:25 +1100

    Add chezmoi

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,7 +30,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = {
+    colorscheme = { "tokyonight", "habamax" },
+  },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
