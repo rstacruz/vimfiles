50c20f9f44fe926435c259c4a7218ac4962db561
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2024-12-14 19:54:53 +1100

    Disable snacks animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.snacks_animate = false
+
 vim.opt.clipboard = "" -- Don't use system clipboard
 vim.opt.swapfile = false -- Don't write swap files
 vim.opt.gdefault = true -- Searches will be global by default
