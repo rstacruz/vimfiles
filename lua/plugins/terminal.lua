8d025800aeefc327edf17328fd27d5a2f709ea3e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-01-21 12:00:23 +1100

    Remove terminal shortcuts

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -11,8 +11,8 @@ return {
       require("toggleterm").setup(opts)
     end,
     keys = {
-      { "<leader>!.", "<cmd>ToggleTerm<cr>", desc = "Terminal (below)" },
-      { "<leader>.", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Terminal (right)" },
+      -- { "<leader>!.", "<cmd>ToggleTerm<cr>", desc = "Terminal (below)" },
+      -- { "<leader>.", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Terminal (right)" },
       { "<leader>gf", '<cmd>w | TermExec size=6 cmd="git-fastsync && exit"<cr>', desc = "Git fastsync" },
     },
   },
