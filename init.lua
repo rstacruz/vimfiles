64c96f824ddceeeb7aa3cbe72400ed2f552a1aa7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-02-02 12:53:53 +1100

    Add workaround for copilotchat termux

--- init.lua
+++ init.lua
@@ -1,4 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- workaround for https://github.com/nvim-lua/plenary.nvim/issues/536
+local is_termux = string.find(vim.loop.os_uname().release, "android")
+if is_termux and os.getenv("XDG_RUNTIME_DIR") == nil then
+  print("XDG_RUNTIME_DIR is not set")
+  local prefix = os.getenv("PREFIX")
+  vim.fn.setenv("XDG_RUNTIME_DIR", prefix .. "/tmp")
+end
+
 pcall(require, "config.local")
 require("config.lazy")
 
