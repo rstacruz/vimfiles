9d9a511a496e5da3a51964e979e652b3388f5eb5
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-02-03 17:27:24 +1100

    Update lua/plugins/mod_lualine.lua

--- lua/plugins/mod_lualine.lua
+++ lua/plugins/mod_lualine.lua
@@ -3,9 +3,18 @@ return {
     "nvim-lualine/lualine.nvim",
     vscode = false,
     opts = function(_, opts)
+      opts.options.component_separators = { left = " ", right = " " }
+      opts.options.section_separators = { left = " ", right = " " }
       opts.sections.lualine_a = {} -- vim mode
       opts.sections.lualine_b = {} -- branch
       opts.sections.lualine_z = {} -- time
+      opts.sections.lualine_x = {
+        opts.sections.lualine_x[1], -- noice cmd
+        opts.sections.lualine_x[2], -- noice status
+        opts.sections.lualine_x[3], -- dap
+        opts.sections.lualine_x[4],
+        opts.sections.lualine_x[5], -- diff
+      }
       return opts
     end,
   },
