1f1496b4022af27a49de38f629db141b586c081a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-03-03 11:00:00 +1100

    Update lazy-lock.json (+1 more changes)

--- lua/plugins/mod_copilot_chat.lua
+++ lua/plugins/mod_copilot_chat.lua
@@ -7,37 +7,33 @@ return {
     },
     opts = function(_, opts)
       opts.prompts = {
-        comblock = {
-          prompt = "Add comments to mark logical sections in the code. If there are any comments that may be outdated, point them out.",
-        },
-        tododraft = {
-          description = "TODO.md: Lint draft",
-          prompt = ""
-            .. "#file:CONVENTIONS.md #file:TODO.md #buffers "
-            .. "I am drafting a plan for a task in TODO.md. "
-            .. "Let me know if the file describes enough info you need. "
-            .. "Keep your response brief. "
-            .. "Avoid giving code examples, only help with requirements. "
-            .. "Try to list down what files and functions may be affected. "
-            .. "Do not make any edits to any files other than TODO.md. ",
-        },
-        todogo = {
-          description = "TODO.md: Run todo",
-          prompt = "" .. "#file:CONVENTIONS.md #file:TODO.md #buffers Perform the changes listed in TODO.md. ",
-        },
-        todoclear = {
-          description = "TODO.md: clear todo",
-          prompt = "" .. "#file:TODO.md " .. "clear out TODO.md, but leave the headings in place.",
-        },
-        sum = {
-          prompt = ""
-            .. "Summarise the following message into bullet points. "
-            .. "Be terse, concise. "
-            .. "Use headings when possible. "
-            .. "Try to keep bullet points to 8 words max unless necessary to convey key info.",
+        ai = {
+          -- mimics Aider's AI behaviour
+          prompt = "#buffer Look for comments in this file marked as AI!. Perform those actions. Then, remove the AI! comment.",
         },
       }
       return opts
+    end,
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+
+      -- Remove what was added by LazyVim (https://www.lazyvim.org/extras/ai/copilot-chat)
+      for i, item in ipairs(opts.right) do
+        if item.ft == "copilot-chat" then
+          table.remove(opts.right, i)
+          break
+        end
+      end
+
+      table.insert(opts.right, {
+        ft = "copilot-chat",
+        title = "Copilot Chat",
+        size = { width = 100 },
+      })
     end,
   },
 }
