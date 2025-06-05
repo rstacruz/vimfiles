d94a653be4cfc637d3d23546851a2613bc8cba2a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-06-05 15:27:01 +1000

    Update lazy-lock.json (+1 more changes)

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,6 +1,8 @@
 return {
   { "olivercederborg/poimandres.nvim", lazy = true },
   { "navarasu/onedark.nvim", lazy = true },
+  { "rebelot/kanagawa.nvim", lazy = true },
   { "projekt0n/github-nvim-theme", lazy = true },
+  { "savq/melange-nvim", lazy = true },
   -- { "sainnhe/edge", lazy = true },
 }
