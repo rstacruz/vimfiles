948e056c77449a5631960d396b9d87fb56759321
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-06-16 16:02:25 +1000

    Update lazy-lock.json (+1 more changes)

---
+++ lua/plugins/osc11.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "afonsofrancof/osc11.nvim",
+    vscode = false,
+    opts = {
+      on_dark = function()
+        vim.notify("Dark mode")
+        vim.opt.background = "dark"
+      end,
+      on_light = function()
+        vim.notify("Light mode")
+        vim.opt.background = "light"
+      end,
+    },
+  },
+}
