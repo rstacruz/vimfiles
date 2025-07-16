61f9f5eeed7895be4fc0e7902e4eb666ccca49d5
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-07-16 10:28:02 +1000

    fix: disable vim.notify in osc11.lua

--- lua/plugins/osc11.lua
+++ lua/plugins/osc11.lua
@@ -4,11 +4,11 @@ return {
     vscode = false,
     opts = {
       on_dark = function()
-        vim.notify("Dark mode")
+        -- vim.notify("Dark mode")
         vim.opt.background = "dark"
       end,
       on_light = function()
-        vim.notify("Light mode")
+        -- vim.notify("Light mode")
         vim.opt.background = "light"
       end,
     },
