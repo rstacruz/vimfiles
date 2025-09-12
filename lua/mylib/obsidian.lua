7c4a2be00f3f7e21a6309a533e628ccd838d6bde
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-09-13 09:39:11 +1000

    feat(obsidian): implement note_frontmatter_func and wire into options

--- lua/mylib/obsidian.lua
+++ lua/mylib/obsidian.lua
@@ -1,5 +1,5 @@
-local M = {}
-
+ local M = {}
+ 
 M.get_obsidian_workspaces = function()
 	local workspaces = {}
 	local home = os.getenv("HOME")
@@ -16,7 +16,7 @@ M.get_obsidian_workspaces = function()
 
 	return workspaces
 end
-
+ 
 M.bind_keys = function(buf)
 	local function extract_to()
 		local new_name = vim.fn.input("New name: ", "", "file")
@@ -47,7 +47,7 @@ M.bind_keys = function(buf)
 	-- vim.keymap.set("n", "gr", "<cmd>Obsidian backlinks<CR>", { buffer = buf, desc = "Obsidian: Show backlinks" })
 	-- stylua: ignore end
 end
-
+ 
 M.get_obsidian_options = function()
 	local opts = {
 		legacy_commands = false,
@@ -61,41 +61,7 @@ M.get_obsidian_options = function()
 		preferred_link_style = "wiki",
 		picker = { name = "snacks.pick" },
 
-		-- note_frontmatter_func = function(note)
-		-- 	local out = {}
-		--
-		-- 	if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-		-- 		for k, v in pairs(note.metadata) do
-		-- 			out[k] = v
-		-- 		end
-		-- 	end
-		--
-		-- 	-- Add createdAt: in the frontmatter
-		-- 	if out.createdAt == nil then
-		-- 		-- "2405 Page" -> 2024-05-01
-		-- 		local datestamp = os.date("!%Y%m")
-		-- 		---@cast datestamp string
-		-- 		if string.match(note.id, "^%d%d%d%d ") and string.sub(note.id, 1, 4) ~= string.sub(datestamp, 3, 9) then
-		-- 			local year = "20" .. string.sub(note.id, 1, 2)
-		-- 			local month = string.sub(note.id, 3, 4)
-		-- 			local ymd = "" .. year .. "-" .. month .. "-01T00:00:00Z"
-		-- 			out.createdAt = ymd
-		-- 		else
-		-- 			out.createdAt = os.date("!%Y-%m-%dT%TZ")
-		-- 		end
-		-- 	end
-		--
-		-- 	-- -- Only save aliases if they're not the same as [self]
-		-- 	-- if note.aliases and next(note.aliases) ~= nil and (#note.aliases ~= 1 or note.aliases[1] ~= note.id) then
-		-- 	--   out.aliases = note.aliases
-		-- 	-- end
-		--
-		-- 	-- if note.tags and next(note.tags) ~= nil then
-		-- 	--   out.tags = note.tags
-		-- 	-- end
-		--
-		-- 	return out
-		-- end,
+		note_frontmatter_func = M.note_frontmatter_func,
 
 		note_id_func = M.note_id_func,
 
@@ -112,7 +78,7 @@ M.get_obsidian_options = function()
 
 	return opts
 end
-
+ 
 M.note_id_func = function(raw_title)
 	-- Default behaviour: return something like "124351678905-XYZX"
 	if raw_title then
@@ -122,5 +88,42 @@ M.note_id_func = function(raw_title)
 
 	return "Untitled-" .. tostring(os.time())
 end
+ 
+M.note_frontmatter_func = function(note)
+	local out = {}
 
+	if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+		for k, v in pairs(note.metadata) do
+			out[k] = v
+		end
+	end
+
+	-- Add createdAt: in the frontmatter
+	if out.createdAt == nil then
+		-- "2405 Page" -> 2024-05-01
+		local datestamp = os.date("!%Y%m")
+		---@cast datestamp string
+		if string.match(note.id, "^%d%d%d%d ") and string.sub(note.id, 1, 4) ~= string.sub(datestamp, 3, 9) then
+			local year = "20" .. string.sub(note.id, 1, 2)
+			local month = string.sub(note.id, 3, 4)
+			local ymd = "" .. year .. "-" .. month .. "-01T00:00:00Z"
+			out.createdAt = ymd
+		else
+			out.createdAt = os.date("!%Y-%m-%dT%TZ")
+		end
+	end
+
+	-- -- Only save aliases if they're not the same as [self]
+	-- if note.aliases and next(note.aliases) ~= nil and (#note.aliases ~= 1 or note.aliases[1] ~= note.id) then
+	--   out.aliases = note.aliases
+	-- end
+
+	-- if note.tags and next(note.tags) ~= nil then
+	--   out.tags = note.tags
+	-- end
+
+	return out
+end
+ 
 return M
+
