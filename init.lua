c70564af6da7cea151d5fd4c42c28f1a37f0d9fd
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2026-02-28 23:47:35 +1100

    feat(init): update copilot NES tab functionality

--- init.lua
+++ init.lua
@@ -112,7 +112,7 @@ now(function() -- options
 		diff = "╱",
 		eob = " ",
 	}
-	vim.o.listchars = "extends:…,nbsp:␣,precedes:…,tab:> "
+	vim.o.listchars = "extends:…,nbsp:␣,precedes:…,tab:  " -- hide tabs
 
 	if vim.fn.has("nvim-0.10") == 1 then
 		vim.opt.smoothscroll = true
@@ -188,6 +188,8 @@ end)
 now(function() -- color scheme
 	add({ source = "rebelot/kanagawa.nvim" })
 	add({ source = "projekt0n/github-nvim-theme" })
+	add({ source = "kshinya7/amaranth.nvim" })
+	add({ source = "followLemmi/cyberneon.nvim" })
 	add({ source = "deparr/tairiki.nvim" }) -- tomorrow-night-like, light and dark versions
 	require("mylib.persist_colorscheme").setup({ fallback = "miniautumn" })
 end)
@@ -507,13 +509,6 @@ later(function() -- editor: lsp features (blink, mason, lspconfig)
 			preset = "super-tab",
 			["<Tab>"] = {
 				function(cmp)
-					if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-						cmp.hide()
-						return (
-							require("copilot-lsp.nes").apply_pending_nes()
-							and require("copilot-lsp.nes").walk_cursor_end_edit()
-						)
-					end
 					if cmp.snippet_active() then
 						return cmp.accept()
 					else
@@ -926,25 +921,7 @@ later(function() -- copilot
 	add({ source = "fang2hou/blink-copilot" })
 	add({ source = "copilotlsp-nvim/copilot-lsp" })
 
-	vim.g.copilot_nes_debounce = 500
 	vim.lsp.enable("copilot_ls")
-	vim.keymap.set("n", "<tab>", function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local state = vim.b[bufnr].nes_state
-		if state then
-			-- Try to jump to the start of the suggestion edit.
-			-- If already at the start, then apply the pending suggestion and jump to the end of the edit.
-			local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
-				or (
-					require("copilot-lsp.nes").apply_pending_nes()
-					and require("copilot-lsp.nes").walk_cursor_end_edit()
-				)
-			return nil
-		else
-			-- Resolving the terminal's inability to distinguish between `TAB` and `<C-i>` in normal mode
-			return "<C-i>"
-		end
-	end, { desc = "Accept Copilot NES suggestion", expr = true })
 
 	-- add({ source = "zbirenbaum/copilot.lua" })
 	-- require("copilot").setup({
